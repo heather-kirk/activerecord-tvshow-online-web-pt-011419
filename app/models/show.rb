0a91b48d803df5eba98087530b4ecf774aea7ae9
Heather Kirk <hskirk@gmail.com>
2019-04-10 02:41:18 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,7 +6,7 @@ class Show < ActiveRecord::Base
   end 
   
   def self.most_popular_show
-    show = Show.maximum(rating: 'DESC').first
+    show = Show.order(rating: 'DESC').first
     show 
   end 
   
