ca9548502a0e1588d758a0385fcc07094b276e5a
Heather Kirk <hskirk@gmail.com>
2019-04-10 02:20:20 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,14 +1,14 @@
 class Show < ActiveRecord::Base
   
   def highest_rating
-    Show.maximum(:rating)
+  show = Show.maximum(:rating)
   end 
   
   def most_popular_show
   end 
   
   def lowest_rating
-    Show.minimum(:rating)
+    show = Show.minimum(:rating)
   end 
   
   def least_popular_show
