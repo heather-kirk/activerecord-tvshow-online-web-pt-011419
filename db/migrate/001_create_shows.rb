fbb6c38d8f1fe27539f19601a707d0f75b0cafcd
Heather Kirk <hskirk@gmail.com>
2019-04-10 02:01:53 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,1 +1,11 @@
-CreateShows > ActiveRecord
+class CreateShows > ActiveRecord::Migration[5.2]
+  
+  def change
+    create_table shows (
+      t.string :name
+      t.string :network
+      t.string :day
+      t.integer :rating )
+  end 
+  
+end 
