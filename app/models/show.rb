ba928528f8d4377002aa4d0e629ccfe0c2e11438
Heather Kirk <hskirk@gmail.com>
2019-04-10 02:40:31 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -16,7 +16,7 @@ class Show < ActiveRecord::Base
   end 
   
   def self.least_popular_show
-        show = Show.minimum(rating: 'ASC').last
+        show = Show.minimum(rating: :ASC).last
         show
   end 
   
